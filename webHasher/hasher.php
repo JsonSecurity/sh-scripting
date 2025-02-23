9fa4876951090a55414b5d73b62d6cf18ccc5619
Json Security <86402253+JsonSecurity@users.noreply.github.com>
2025-02-22 22:12:34 -0500

    Update hasher.php

--- webHasher/hasher.php
+++ webHasher/hasher.php
@@ -6,35 +6,35 @@
         $BCRYPT = password_hash($password, PASSWORD_BCRYPT);
         echo "[+] BCRYPT: $BCRYPT\n";
 
-        // MD5
+        // MD5  
         $MD5 = md5($password);
         echo "[+] MD5: $MD5\n";
 
-        // SHA-1
+        // SHA-1  
         $SHA1 = sha1($password);
         echo "[+] SHA1: $SHA1\n";
 
-        // SHA-256
+        // SHA-256  
         $SHA256 = hash('sha256', $password);
         echo "[+] SHA256: $SHA256\n";
 
-        // SHA-512
+        // SHA-512  
         $SHA512 = hash('sha512', $password);
         echo "[+] SHA512: $SHA512\n";
 
-        // WHIRLPOOL
+        // WHIRLPOOL  
         $WHIRLPOOL = hash('whirlpool', $password);
         echo "[+] WHIRLPOOL: $WHIRLPOOL\n";
 
-        // RIPMD-160
+        // RIPMD-160  
         $RIPMD160 = hash('ripemd160', $password);
         echo "[+] RIPEMD-160: $RIPMD160\n";
 
-        // TIGER-192
+        // TIGER-192  
         $TIGER192 = hash('tiger192,3', $password);
         echo "[+] TIGER-192: $TIGER192\n";
 
-        // CRC32
+        // CRC32  
         $CRC32 = hash('crc32', $password);
         echo "[+] CRC32: $CRC32\n";
 
